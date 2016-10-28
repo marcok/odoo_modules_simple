fea7ee0e3aab6bb1766abd1b1a7ad183450df5e7
Anatolij Yakymets <anatoliy.yakymets@gmail.com>
2016-10-28 14:30:00 +0300

    Init

--- hosberg_custom/models/hr_applicant.py
+++ hosberg_custom/models/hr_applicant.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import osv, fields
+from openerp import api, fields, models, _
 
-class hr_job(osv.osv):
+
+class hr_job(models.Model):
     _inherit = ['hr.applicant']
 
-    _columns = {
-        'salutation':fields.char('salutation (mr. or mrs.)'),
-        'first_name': fields.text('First Name', size=250),
-        'last_name': fields.text('Last Name', size=250),
-        'partner_address': fields.char('Address', length=250),
-        'place': fields.text('Place', size=250),
-        'cover_letter': fields.binary('Cover Letter'),
-        'curriculum_vitae': fields.binary('Curriculum Vitae'),
-        'diploma_certificates': fields.binary('Diploma Certificates'),
-        'possible_join_date':fields.char('Possible Join Date')
-    }
+    salutation = fields.Char(string='salutation (mr. or mrs.)')
+    first_name = fields.Text(string='First Name', size=250)
+    last_name = fields.Text(string='Last Name', size=250)
+    partner_address = fields.Char(string='Address', length=250)
+    place = fields.Text(string='Place', size=250)
+    cover_letter = fields.Binary(string='Cover Letter')
+    curriculum_vitae = fields.Binary(string='Curriculum Vitae')
+    diploma_certificates = fields.Binary(string='Diploma Certificates')
+    possible_join_date = fields.Char(string='Possible Join Date')
